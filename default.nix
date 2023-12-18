a9d26eded07c32c32f135e7604fa5f5e09e48059
yue <yorke5@qq.com>
2023-12-18 16:48:07 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
  rpkgs = builtins.attrValues {
-  inherit (pkgs.python3) numpy matplotlib;
+  inherit (pkgs.python311Packages) numpy matplotlib;
 };
 in
   pkgs.mkShell {
