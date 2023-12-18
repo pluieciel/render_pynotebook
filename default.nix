bba1d1855d4959bbb93d54ca881507a21163c07d
yue <yorke5@qq.com>
2023-12-18 16:45:15 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,18 @@
-environment.systemPackages = [
-  python3
-];
-
 let
-  my-python-packages = ps: with ps; [
-    numpy
-    matplotlib
-  ];
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
+ rpkgs = builtins.attrValues {
+  inherit (pkgs.python3) numpy matplotlib;
+};
 in
-environment.systemPackages = [
-  (pkgs.python3.withPackages my-python-packages)
-];
+  pkgs.mkShell {
+    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+
+    buildInputs = [  rpkgs ];
+      
+  }
