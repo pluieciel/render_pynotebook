a76bf64485cceed57ef0998498fb220e7dd4ac78
yue <yorke5@qq.com>
2023-12-18 16:59:59 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
  rpkgs = builtins.attrValues {
-  inherit (pkgs.python311Packages) numpy matplotlib jupyter nbconvert pandoc;
+  inherit (pkgs.python311Packages) numpy matplotlib jupyter nbconvert;
 };
 in
   pkgs.mkShell {
