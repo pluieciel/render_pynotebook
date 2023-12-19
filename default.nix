a5975cbd94d900c1f04f547ad55b8fc3c3daa286
yue <yorke5@qq.com>
2023-12-19 09:58:14 +0100

    revert

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
- rpkgs = builtins.attrValues {
+ pypkgs = builtins.attrValues {
   inherit (pkgs.python311Packages) numpy matplotlib jupyter nbconvert;
 };
 in
@@ -13,5 +13,5 @@ in
     LC_PAPER = "en_US.UTF-8";
     LC_MEASUREMENT = "en_US.UTF-8";
 
-    buildInputs = [ pandoc  rpkgs ];
+    buildInputs = [ pypkgs ];
   }
