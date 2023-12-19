894fa055d1fd9db6e614caa4acf0f85be238d622
yue <yorke5@qq.com>
2023-12-19 10:17:58 +0100

    latex

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
  pypkgs = builtins.attrValues {
   inherit (pkgs.python311Packages) numpy matplotlib jupyter nbconvert;
+ texlive = pkgs.texlive.combined.scheme-full
 };
 in
   pkgs.mkShell {
@@ -13,5 +14,5 @@ in
     LC_PAPER = "en_US.UTF-8";
     LC_MEASUREMENT = "en_US.UTF-8";
 
-    buildInputs = [ pypkgs texlive.combined.scheme-full ];
+    buildInputs = [ pypkgs texlive ];
   }
